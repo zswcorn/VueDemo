819e8f32e6727b1a2965c7e413d713fb3be12f8e
zswcorn <zswcorn@gmail.com>
2016-10-28 16:32:29 +0800

    修改代码结构

--- Content/JS/vueDemo.js
+++ Content/JS/vueDemo.js
@@ -1,62 +1,77 @@
-var markDown = new Vue({
-	el: "#editor",
-	data: {
-		input: "# hello-Daemon_W_Geek's Garage"
-	},
-	computed: {
-		compiledMarkdown: function () {
-			return marked(this.input, {
-				sanitize: true
-			})
-		}
-	},
-	methods: {
-		update: function (e) {
-			this.input = e.target.value
-		}
-	}
-});
-var helloApp = new Vue({
-	el: "#Hello",
-	data: {
-		Message: "Hello-Daemon",
-		title: new Date()
-	}
-});
-var listApp = new Vue({
-	el: "#dataList",
-	data: {
-		todos: [{
-			text: "ZSW"
-		}, {
-			text: "Daemon"
-		}, {
-			text: "Both"
-		}, ]
-	}
-});
-var ifApp = new Vue({
-	el: "#ifApp",
-	data: {
-		seen: true
-	}
-});
-var methodApp = new Vue({
-	el: "#methodApp",
-	data: {
-		inputDisplay: "No Input!"
-	},
-	methods: {
-		inputChange: function (e) {
-			this.inputDisplay = e.target.value;
-		}
-	}
+$(document).ready(function () {
+	createMarkdown();
+	dataBind();
+	forBind();
+	ifBind();
+	methodBind();
+	
 });
 
-$(document).ready(function () {
-	$("body>*").on("mouseenter", function (e) {
-		console.log(e.target.nodeName);
-		//iframe获取控件
-		//$(document.getElementById("iframeId").contentWindow.document).find("*").on("mouseenter",function(e){console.log(e.target.nodeName+"frame")})
+function createMarkdown() {
+	var markDown = new Vue({
+		el: "#editor",
+		data: {
+			input: "# hello-Daemon_W_Geek's Garage"
+		},
+		computed: {
+			compiledMarkdown: function () {
+				return marked(this.input, {
+					sanitize: true
+				})
+			}
+		},
+		methods: {
+			update: function (e) {
+				this.input = e.target.value
+			}
+		}
 	});
-});
+};
+
+function dataBind() {
+	var helloApp = new Vue({
+		el: "#twoWayBinding",
+		data: {
+			Message: "Hello-Daemon",
+			title: new Date()
+		}
+	});
+};
+
+function forBind() {
+	var listApp = new Vue({
+		el: "#dataList",
+		data: {
+			todos: [{
+				text: "ZSW"
+			}, {
+				text: "Daemon"
+			}, {
+				text: "Both"
+			}, ]
+		}
+	});
+};
+
+function ifBind() {
+	var ifApp = new Vue({
+		el: "#ifApp",
+		data: {
+			seen: true
+		}
+	});
+};
+
+function methodBind() {
+	var methodApp = new Vue({
+		el: "#methodApp",
+		data: {
+			inputDisplay: "No Input!"
+		},
+		methods: {
+			inputChange: function (e) {
+				this.inputDisplay = e.target.value;
+			}
+		}
+	});
+};
